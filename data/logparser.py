550b27879472a83183a640c438625ad76d3f90b1
Erik Karulf <erik@karulf.com>
2010-04-22 14:57:38 -0700

    Initial version of logparser.py

---
+++ data/logparser.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+import re
+import sys
+import time
+import calendar
+# Counters
+FPS_TIME  = 0.0
+TPS_TIME  = 0.0
+RTS_TIME  = 0.0
+
+class Robot(object):
+    IDLE, MOVING, DIRECT = range(3)
+    
+    def __init__(self, epoch):
+        self.state = None
+        self.state_begin = None
+        self.state_times = {}
+        for state in (Robot.IDLE, Robot.MOVING, Robot.DIRECT):
+            self.state_times[state] = 0.0
+        
+        self.neglect_begin = None
+        self.neglect_time = 0.0
+
+        self.selected = False
+        
+        self.set_state(Robot.IDLE, epoch)
+        self.set_selected(False, epoch)
+    
+    def set_state(self, state, timestamp):
+        if not self.state == state:
+            if self.state in self.state_times:
+                self.state_times[self.state] += timestamp - self.state_begin
+            self.state = state
+            self.state_begin = timestamp
+        self.update_neglected(timestamp)
+    
+    def set_selected(self, selected, timestamp):
+        if not self.selected == selected:
+            self.selected = selected
+        self.update_neglected(timestamp)
+    
+    def update_neglected(self, timestamp):
+        neglected = self.is_neglected()
+        if neglected and self.neglect_begin is None:
+            self.neglect_begin = timestamp
+        elif not neglected and self.neglect_begin is not None:
+            self.neglect_time += timestamp - self.neglect_begin
+            self.neglect_begin = None
+        
+    def is_neglected(self):
+        return self.state not in (Robot.MOVING, Robot.DIRECT) and not self.selected
+            
+
+class UI(object):
+    RTS, TPS, FPS = range(3)
+    
+    def __init__(self, epoch):
+        self.state = None
+        self.state_times = {}
+        for state in (UI.RTS, UI.TPS, UI.FPS):
+            self.state_times[state] = 0.0
+        self.begin = None
+        self.switches = 0
+        self.set_state(UI.RTS, epoch)
+
+    def set_state(self, state, timestamp):
+        if not self.state == state:
+            if self.state in self.state_times:
+                self.state_times[self.state] += timestamp - self.begin
+            self.state = state
+            self.begin = timestamp
+            self.switches += 1
+
+def main(filename):
+    robots = {}
+    initialized = False
+    selected = []
+    epoch = None
+    with open(filename, 'r') as f:
+        for lineno, line in enumerate(f.readlines()):
+            fields = line.strip().split('\t')
+            try:
+                timestamp = calendar.timegm(time.strptime(fields[0], "%H:%M:%S"))
+                msg_type  = fields[1]
+                extra     = fields[2] if len(fields) >= 2 else ""
+            except IndexError:
+                print "Skipping line %d" % lineno
+                continue
+            # Initialize!
+            if not initialized:
+                for name in ["Blood", "Sweat"]:
+                    robots[name] = Robot(timestamp)
+                ui = UI(timestamp)
+                epoch = timestamp
+                initialized = True
+            # Switch based on message type
+            #
+            # General Messages
+            #
+            if msg_type == 'mode switched to':
+                if extra == 'TPD':
+                    ui.set_state(UI.TPS, timestamp)
+                elif extra == 'FPD':
+                    ui.set_state(UI.FPS, timestamp)
+                elif extra == 'Supervisor':
+                    ui.set_state(UI.RTS, timestamp)
+                else:
+                    print "BAD HOODOO! (invalid mode on line %d)" % lineno
+                    sys.exit(1)
+            elif msg_type == 'selection changed to':
+                old_selected = selected
+                selected = []
+                for name in extra.split(', '):
+                    try:
+                        selected.append(robots[name])
+                    except KeyError:
+                        continue
+                # Deselect old robots
+                for robot in old_selected:
+                    if robot not in selected:
+                        robot.set_selected(False, timestamp)
+                # Select new robots
+                for robot in selected:
+                    if robot not in old_selected:
+                        robot.set_selected(True, timestamp)
+            elif msg_type == 'notification event':
+                match = re.match(r'^\[\w+\] (\w+): Movement', extra)
+                if match:
+                    try:
+                        robot = robots[match.group(1)]
+                    except KeyError:
+                        continue
+                    else:
+                        robot.set_state(Robot.IDLE, timestamp)
+            #
+            # Supervisor Messages
+            #
+            elif msg_type == 'done issuing task' and extra == 'Move':
+                for robot in selected:
+                    robot.set_state(Robot.MOVING, timestamp)
+            #
+            # Third / First Person Messages
+            #
+            elif msg_type in ('third-person direct mode event',
+                              'first-person direct mode event'):
+                assert len(selected) == 1, "Should only be one robot selected"
+                if re.match("^arrow_(\w+)$", extra):
+                    for robot in selected:
+                        robot.set_state(Robot.DIRECT, timestamp)
+                elif re.match("^arrow_(\w+)_up$", extra):
+                    for robot in selected:
+                        robot.set_state(Robot.IDLE, timestamp)
+            else:
+                print "No handler for Line %d: %s" % (lineno, repr(fields))
+    # Finished parsing file, close state
+    # Relies on timestamp fallthrough from for loop... bad style
+    ui.set_state(None, timestamp)
+    for robot in robots.values():
+        robot.set_selected(False, timestamp)
+        robot.set_state(None, timestamp)
+    # Print Interesting Stats Here!
+    print
+    print "Total Time: %d" % (timestamp - epoch)
+    print \
+"""UI Stats
+--------
+Supervisor Time: %d
+3rd Direct Time: %d
+1st Direct Time: %d
+Mode Changes:    %d
+""" % (ui.state_times[UI.RTS], ui.state_times[UI.TPS], ui.state_times[UI.FPS], ui.switches)
+
+    for name, robot in robots.items():
+        print \
+"""%s
+--------
+Neglect Time: %d
+Idle Time:    %d
+Moving Time:  %d
+Direct Time:  %d
+""" % (name, robot.neglect_time, robot.state_times[Robot.IDLE], 
+       robot.state_times[Robot.MOVING], robot.state_times[Robot.DIRECT])
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        main(filename)
+    else:
+        print "usage: %s filename" % sys.argv[0]
