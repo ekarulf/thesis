1040a4e38cdc31e70fd4597a7a5e5a2794c597d4
Erik Karulf <erik@karulf.com>
2010-04-23 19:23:36 -0700

    Insert into GDoc

--- data/logparser.py
+++ data/logparser.py
@@ -3,6 +3,9 @@ import re
 import sys
 import time
 import calendar
+import gdata.spreadsheet.service
+import os
+
 # Counters
 FPS_TIME  = 0.0
 TPS_TIME  = 0.0
@@ -74,6 +77,13 @@ class UI(object):
             self.switches += 1
 
 def main(filename):
+    m = re.match("./(\d{2}\-\d{2})/(\w+)/[^\[]+\[(\d+)\]", filename)
+    if m:
+        user_id, experiment, run_id = m.group(1, 2, 3)
+    else:
+        user_id = filename
+        run_id = experiment = None
+    
     robots = {}
     initialized = False
     selected = []
@@ -183,6 +193,50 @@ Moving Time:  %d
 Direct Time:  %d
 """ % (name, robot.neglect_time, robot.state_times[Robot.IDLE], 
        robot.state_times[Robot.MOVING], robot.state_times[Robot.DIRECT])
+    
+    # INSERT INTO SPREADSHEET
+    SpreadsheetInsert({
+        'blooddirects': robots["Blood"].state_times[Robot.DIRECT],
+        'bloodidles': robots["Blood"].state_times[Robot.IDLE],
+        'bloodmovings': robots["Blood"].state_times[Robot.MOVING],
+        'bloodneglects': robots["Blood"].neglect_time,
+        'completion': timestamp - epoch,
+        'experiment': experiment,
+        'id': user_id,
+        'modeswitches': ui.switches,
+        'run': run_id,
+        'sweatdirects': robots["Sweat"].state_times[Robot.DIRECT],
+        'sweatidles': robots["Sweat"].state_times[Robot.IDLE],
+        'sweatmovings': robots["Sweat"].state_times[Robot.MOVING],
+        'sweatneglects': robots["Sweat"].neglect_time,
+        'timein1sts': ui.state_times[UI.FPS],
+        'timein3rds': ui.state_times[UI.TPS],
+        'timeinsupervisors': ui.state_times[UI.RTS],
+        'timetobox1': None,
+        'timetobox2': None,
+        'timetobox3': None,
+        'totaldirecttimes': robots["Sweat"].state_times[Robot.DIRECT] + robots["Blood"].state_times[Robot.DIRECT],
+        'totalidletimes': robots["Sweat"].state_times[Robot.IDLE] + robots["Blood"].state_times[Robot.IDLE],
+        'totalmovingtimes': robots["Sweat"].state_times[Robot.MOVING] + robots["Blood"].state_times[Robot.MOVING],
+        'totalneglecttimes': robots["Sweat"].neglect_time + robots["Blood"].neglect_time,
+        'typeierrors': None,
+        'typeiierrors': None,
+    })
+
+def SpreadsheetInsert(data):
+    gd_client = gdata.spreadsheet.service.SpreadsheetsService()
+    gd_client.email = 'ekarulf@gmail.com'
+    gd_client.password = os.getenv("GMAIL_PASSWORD")
+    gd_client.source = "FooBar"
+    gd_client.ProgrammaticLogin()
+    
+    SPREADSHEET_ID = '0AuFk0R2ReTQHdE9JZHFBc0M4NmpndlQ4U2RNcmh3ZVE'
+    WORKSHEET_ID = 'oda'
+    for key, value in data.items():
+        if value is not None and not type(value) == str:
+            data[key] = str(value)
+    gd_client.InsertRow(data, SPREADSHEET_ID, WORKSHEET_ID)
+    
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
